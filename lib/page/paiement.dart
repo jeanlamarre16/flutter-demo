e6173a3ff1ec02d4d3c9f80c07bc52021c662376
AJL <jeanlamarre16@yahoo.fr>
2024-01-12 11:16:21 +0100

    border-radius

--- lib/page/paiement.dart
+++ lib/page/paiement.dart
@@ -14,62 +14,93 @@ class Paiement extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
-              child: Column(
+              margin: const EdgeInsets.all(5.0),
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                border: Border.all(),
+                borderRadius: BorderRadius.circular(20.0),
+            ),
+
+              child: const Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   Text(
                     "Récapitulatif de votre commande",
                     style: TextStyle(
-                      fontSize: 24.0
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold
                     )
                 ),
-                  Row(
-                    children: [
-                      Text(
-                        textAlign: TextAlign.right,
-                        "125",
-                          style: TextStyle(
-                              color: Colors.green, // Couleur du texte
-                              fontSize: 24.0)
-                      ),
-                    ],
-                  ),
-
-                  Row(
-                    children: [
-                      Text(
+                Row(
+                  children: [
+                    Text(
                         "Sous-total",
-                          style: TextStyle(
-                          fontSize: 24.0)
-                      ),
-                      Text(
-                        "vous économisez",
-                          style: TextStyle(
-                              color: Colors.green, // Couleur du texte
-                              fontSize: 24.0)
-                      ),
-                    ],
-                  ),
+                        style: TextStyle(
+                            fontSize: 16.0)
+                    ),
+                    Text(
+                      textAlign: TextAlign.right,
+                      "0.00",
+                        style: TextStyle(
+                            fontSize: 16.0)
+                    ),
+                  ],
+                ),
 
-                  Row(
-                    children: [
-                      Text(
-                        "TVA",
-                          style: TextStyle(
-                              fontSize: 24.0
-                          )
-                      ),
-                      Text(
-                        "Total",
-                          style: TextStyle(
-                              fontSize: 24.0
-                          )
-                      ),
-                    ],
-                  ),
+                Row(
+                  children: [
+                    Text(
+                      "vous économisez",
+                        style: TextStyle(
+                            color: Colors.green, // Couleur du texte
+                            fontSize: 16.0)
+                    ),
+                    Text(
+                        "0.00",
+                        style: TextStyle(
+                            color: Colors.green, // Couleur du texte
+                            fontSize: 16.0)
+                    ),
+                  ],
+                ),
+
+                Row(
+                  children: [
+                    Text(
+                      "TVA",
+                        style: TextStyle(
+                            fontSize: 16.0
+                        )
+                    ),
+                    Text(
+                      "0.00",
+                        style: TextStyle(
+                            fontSize: 16.0
+                        )
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                        "TOTAL",
+                        style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold
+
+                        )
+                    ),
+                    Text(
+                        "0.00",
+                        style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold
+                        )
+                    ),
+                  ],
+                ),
                 ],
               ),
             ),
@@ -77,6 +108,18 @@ class Paiement extends StatelessWidget {
               height: 8.0,
             ),
             const Spacer(),
+            Text(
+                "En cliquant sur \"Confirmer l'achat\" vous acceptez les conditions de ventes d'EPSI Shop International. Besoin d'aide ? Désolé on ne peut rien faire.",
+                style: TextStyle(
+                    fontSize: 12.0
+                )
+            ),
+            Text(
+                "En poursuivant vous acceptez les conditions d'utilisation du fournisseur du paiement CoffeeDis",
+                style: TextStyle(
+                    fontSize: 12.0
+                )
+            ),
             ElevatedButton(
                 child: Text("Confirmer l'achat"),
                 onPressed: ()=> context.go("/validation"),
